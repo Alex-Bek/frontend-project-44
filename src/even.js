14700d8b33f86fff37d10bf20b703934f9246d69
Your Name <you@example.com>
2024-04-22 22:45:00 +0300

    Lint new files

--- src/even.js
+++ src/even.js
@@ -1,31 +1,29 @@
-import { askQuestion, recieveAnswer, answerEvaluation} from './index.js'
- 
+import { askQuestion, recieveAnswer, answerEvaluation } from './index.js';
+
 const generateQuestion = () => {
   const isEven = (num) => num % 2 === 0;
   const randNum = Math.floor(Math.random() * (100 + 1));
-  const correctAnswer = isEven(randNum) ? 'yes' : 'no'
+  const correctAnswer = isEven(randNum) ? 'yes' : 'no';
   return [randNum, correctAnswer];
-}
+};
 
-const validateInput = (answer) => {
-  return ['yes', 'no'].includes(answer)
-}
+const validateInput = (answer) => ['yes', 'no'].includes(answer);
 
 const evenGame = (name) => {
   let successfulAttempts = 0;
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   while (successfulAttempts < 3) {
-    const [question, correctAnswer] = generateQuestion();   
-    askQuestion(question) 
-    const answer = recieveAnswer()
+    const [question, correctAnswer] = generateQuestion();
+    askQuestion(question);
+    const answer = recieveAnswer();
 
     if (!validateInput(answer)) {
       console.log('wrong input');
       return false;
     }
     if (!answerEvaluation(answer, correctAnswer)) {
-      return false
+      return false;
     }
     successfulAttempts += 1;
   }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import readlineSync from 'readline-sync';
 
 export const askQuestion = (question) => {
-  console.log(`Question: ${question}`)
-}
+  console.log(`Question: ${question}`);
+};
 
-export const recieveAnswer = () => {
-  return readlineSync.question('Your answer: ').toLowerCase();
-}
+export const recieveAnswer = () => readlineSync.question('Your answer: ').toLowerCase();
 
 export const answerEvaluation = (answer, correctAnswer) => {
   if (correctAnswer === answer) {
     console.log('correct');
     return true;
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was ${correctAnswer}.`)
-    return false;
   }
-}
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+  return false;
+};
