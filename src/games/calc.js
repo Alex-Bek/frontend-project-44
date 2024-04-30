04aaf97063d884aa706d1d04c9f4975f12ed109b
Your Name <you@example.com>
2024-04-30 16:18:50 +0300

    Refactor calc to use dispatching

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,21 +1,19 @@
 import greeting from '../cli.js';
 import { playGame, endGame } from '../index.js';
 
-const despatchAction = (arg1, arg2, action) => {
-  if (action === '+') return arg1 + arg2;
-  if (action === '-') return arg1 - arg2;
-  if (action === '*') return arg1 * arg2;
-  return null;
-};
-
 const generateQuestion = () => {
   const genRand = (top = 100, bot = 1) => Math.floor(Math.random() * (top - bot + 1)) + bot;
   const randNum1 = genRand();
   const randNum2 = genRand();
   const actions = ['+', '-', '*'];
   const chosenAction = actions[genRand(2, 0)];
+  const actionFunc = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b
+  };
   const questionLine = `${randNum1} ${chosenAction} ${randNum2}`;
-  const correctAnswer = despatchAction(randNum1, randNum2, chosenAction).toString();
+  const correctAnswer = actionFunc[chosenAction](randNum1, randNum2).toString();
   return [questionLine, correctAnswer];
 };
 
